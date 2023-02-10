0f4cb74ee1089ad3dfacaef8f40e30d59ed74d5b
Matthew Favela <92203491+Chewy42@users.noreply.github.com>
2023-02-10 00:42:51 +0000

    oop refactor

--- bot.py
+++ bot.py
@@ -5,33 +5,39 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-def generateText(prompt, temperature, tokens):
-    res = openai.Completion.create(
-        model="text-davinci-003", 
-        prompt=prompt, 
-        temperature=temperature, 
-        max_tokens=tokens
-    )
-    return res.choices[0].text[2:]
+class Bot():
+    #Important Variables
+    maxIters = 3
+    niche = None
 
-def approve(generatedText):
-    print("Generated Text:")
-    print(generatedText)
-    print("Is this text acceptable?\n")
-    return input()
+    def generateText(prompt, temperature, tokens):
+        res = openai.Completion.create(
+            model="text-davinci-003", 
+            prompt=prompt, 
+            temperature=temperature, 
+            max_tokens=tokens
+        )
+        return res.choices[0].text[2:]
+
+    def approve(generatedText):
+        print("Generated Text:")
+        print(generatedText)
+        print("Is this text acceptable? (yes/no))")
+        return input()
+
+    def generateSupervised(prompt, temperature, max_tokens):
+        hasBeenApproved = False
+        i = 0
+        while hasBeenApproved is False or i >= maxIters:
+            niche = generateText(
+            prompt, 
+            temperature, #temperature
+            max_tokens, #max tokens
+            )
+            if approve(niche).lower() != 'no':
+                hasBeenApproved = True
+            i += 1
+        return niche
 
 
-niche = None
-
-
-hasBeenApproved = False
-maxIters = 3
-i = 0
-while hasBeenApproved is False or i >= maxIters:
-    niche = generateText(
-    "randomly pick either Health and Welness, E-Commerce, or Relationships, then return back ONLY the one you have picked", 
-    0.6, #temperature
-    20) #max tokens
-    if approve(niche).lower() != 'no':
-        hasBeenApproved = True
-    i += 1
+if __name__ == "__main__":
